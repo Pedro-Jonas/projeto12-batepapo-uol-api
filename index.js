c9422b54ad5b087366ae223b8d0bdb2e2d6e8403
jonas <pedrojonas.pj77@gmail.com>
2022-09-03 09:40:03 -0300

    Post /participants implementado

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from 'joi'
+import dayjs from "dayjs";
 
+dayjs().format();
 dotenv.config();
 
 const server = express();
@@ -22,20 +24,40 @@ const nameSchema = joi.object({
 
 server.post("/participants", async (req, res) =>{
     const name = (req.body);
-    db.collection("participantes").find().toArray().then((data) => {
-        for (let i = 0; i < data.length; i++){
-            if (name.name === data[i].name){
-                console.log(data[i].name)
-                res.status(409).send("erro");
+    const validation = nameSchema.validate(name);
+    if (validation.error){
+        res.status(422).send("error");
+        return;
+    };
+    try{
+        const participants = await db.collection("participantes").find().toArray();
+
+        for (let i = 0; i < participants.length; i++){
+            if (name.name === participants[i].name){
+                return res.status(409).send("erro");
             };
         };
-    });
-    const response = await db.collection("participantes").insertOne({
-        ...name,
-        lastStatus: Date.now()
-    });
-
-    res.status(201).send("Ok");
+    } catch(error){
+        res.sendStatus(422);
+    };
+    
+    
+    try{
+        const participant = await db.collection("participantes").insertOne({
+            ...name,
+            lastStatus: Date.now()
+        });
+        const message = await db.collection("mensagens").insertOne({
+            from : name.name,
+            to: "todos",
+            text: "entra na sala",
+            type: "status",
+            time: dayjs().format("HH:mm:ss")
+        });
+        res.status(201).send("Ok");
+    } catch {
+        res.sendStatus(422);
+    };
 
 });
 
